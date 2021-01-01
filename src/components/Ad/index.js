88daba1f9a3d5c47fca1ae442a470aee88d47de8
NentoR <nentormail@gmail.com>
2021-01-01 16:59:38 +0200

    Add AdWrapper to Ad

--- src/components/Ad/index.js
+++ src/components/Ad/index.js
@@ -1,11 +1,16 @@
+import styled from 'styled-components'
 import { Title, AdPostedDate } from './Components'
 import { Card } from './Components'
 
+const AdWrapper = styled.article`
+  padding: 5rem 15rem;
+`
+
 export const Ad = () => {
   return (
-    <>
+    <AdWrapper>
       <AdPostedDate>Posted Dec 29</AdPostedDate>
       <Title>Marketing Director</Title>
-    </>
+    </AdWrapper>
   )
 }
