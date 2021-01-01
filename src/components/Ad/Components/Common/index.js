0067d63a814e266fb91ac0f6e4ecc136d55815f5
NentoR <nentormail@gmail.com>
2021-01-01 16:56:38 +0200

    Add Title and AdPostedDate styled components in Ad Common

---
+++ src/components/Ad/Components/Common/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  color: #212529;
+  padding: 0;
+  margin: 0 0 0.07rem 0;
+  font-weight: normal;
+  line-height: 1.5em;
+  font-family: sans-serif;
+`
+
+export const AdPostedDate = styled.h3`
+  font-size: 0.8rem;
+  color: #4d5154;
+  margin: 0 0 1.4rem 0;
+  line-height: 1em;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  font-family: sans-serif;
+`
--- src/components/Ad/index.js
+++ src/components/Ad/index.js
@@ -1,16 +1,11 @@
-import styled from 'styled-components'
+import { Title, AdPostedDate } from './Components'
 import { Card } from './Components'
 
 export const Ad = () => {
   return (
-    <div>
-      <Card
-        logoUrl="https://crhscountyline.com/wp-content/uploads/2020/03/Capture.png"
-        title="Job Title"
-        location="Berlin"
-        websiteUrl="https://google.com/"
-        adsPosted={55}
-      />
-    </div>
+    <>
+      <AdPostedDate>Posted Dec 29</AdPostedDate>
+      <Title>Marketing Director</Title>
+    </>
   )
 }
